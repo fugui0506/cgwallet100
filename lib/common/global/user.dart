3df551fbd47e84252a73c2760b695fd0f9ea7dec
fugui <fugui0506@gmail.com>
2025-01-07 11:57:26 +0400

    新增退回后台监控

--- lib/common/global/user.dart
+++ lib/common/global/user.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:my_utils/my_utils.dart';
 
-class UserController extends GetxController {
+class UserController extends GetxService with WidgetsBindingObserver {
   static UserController get to => Get.find();
 
   // 初始化等待方法
@@ -23,7 +24,26 @@ class UserController extends GetxController {
   @override
   void onInit() async {
     super.onInit();
+    WidgetsBinding.instance.addObserver(this);
     _initCompleter.complete();
     MyLogger.w('UserController 初始化完毕...');
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    // 监视 App 是否切换到后台
+    switch (state) {
+      case AppLifecycleState.resumed:
+        MyLogger.w('app 切换到了前台');
+        UserController.to.myWss?.connect();
+        break;
+      case AppLifecycleState.paused:
+        MyLogger.w('app 切换到了后台');
+        UserController.to.myWss?.close();
+        break;
+      default:
+        break;
+    }
+    super.didChangeAppLifecycleState(state);
   }
 }
